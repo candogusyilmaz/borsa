194882d9c88e075b2baca7793db73d1cfd63248e
candogusyilmaz <candogusyilmaz@gmail.com>
2025-02-05 23:58:57 +0300

    setup

--- src/main/java/dev/canverse/stocks/dataloader/Setup.java
+++ src/main/java/dev/canverse/stocks/dataloader/Setup.java
@@ -1,5 +1,6 @@
 package dev.canverse.stocks.dataloader;
 
+import dev.canverse.stocks.repository.CountryRepository;
 import dev.canverse.stocks.repository.ExchangeRepository;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -13,17 +14,39 @@ import org.springframework.util.FileCopyUtils;
 
 import java.io.IOException;
 
-@Order(501)
+@Order(-1)
 @Component
 @RequiredArgsConstructor
-public class ExchangesDataLoader implements CommandLineRunner {
-    private static final Logger log = LoggerFactory.getLogger(ExchangesDataLoader.class);
+public class Setup implements CommandLineRunner {
+    private static final Logger log = LoggerFactory.getLogger(Setup.class);
 
     private final JdbcTemplate jdbcTemplate;
+    private final CountryRepository countryRepository;
     private final ExchangeRepository exchangeRepository;
 
     @Override
     public void run(String... args) {
+        setupCountries();
+        setupExchanges();
+    }
+
+    private void setupCountries() {
+        if (countryRepository.count() > 0) {
+            log.info("Countries data already loaded.");
+            return;
+        }
+
+        try {
+            // Get the SQL file from resources
+            var resource = new ClassPathResource("/scripts/countries.sql");
+            var sqlScript = new String(FileCopyUtils.copyToByteArray(resource.getInputStream()));
+            jdbcTemplate.execute(sqlScript);
+        } catch (IOException e) {
+            log.error("Error reading SQL file: " + e.getMessage());
+        }
+    }
+
+    private void setupExchanges() {
         if (exchangeRepository.count() > 0) {
             log.info("Exchanges data already loaded.");
             return;
