c65dca278af4a8c3a6e5767741fc5bef071c9f4a
candogusyilmaz <candogusyilmaz@gmail.com>
2025-02-12 00:45:55 +0300

    trades heatmap update no records display

--- src/main/web/src/components/Portfolio/TradesHeatMap.tsx
+++ src/main/web/src/components/Portfolio/TradesHeatMap.tsx
@@ -1,15 +1,17 @@
-import { Box, Card, Group, Stack, Text, Tooltip, rem } from '@mantine/core';
-import { IconCircle } from '@tabler/icons-react';
-import { useSuspenseQuery } from '@tanstack/react-query';
-import { queries } from '~/api';
-import { constants } from '~/lib/constants';
-import { format } from '~/lib/format';
+import { Box, Card, Group, Stack, Text, Tooltip, rem } from "@mantine/core";
+import { IconCircle } from "@tabler/icons-react";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { queries } from "~/api";
+import { constants } from "~/lib/constants";
+import { format } from "~/lib/format";
 
 export function TradesHeatMap() {
   const { data } = useSuspenseQuery(queries.trades.heatMap());
 
   const monthShortNames = Array.from({ length: 12 }, (_, index) =>
-    new Intl.DateTimeFormat(constants.locale(), { month: 'short' }).format(new Date(2000, index, 1))
+    new Intl.DateTimeFormat(constants.locale(), { month: "short" }).format(
+      new Date(2000, index, 1)
+    )
   );
 
   const months = Array.from({ length: 12 }, (_, i) => i + 1);
@@ -23,6 +25,25 @@ export function TradesHeatMap() {
     return { year: yearData.year, data: filledMonths };
   });
 
+  if (data.length === 0) {
+    return (
+      <>
+        <Text fw={700} size={rem(22)}>
+          Monthly Revenue Overview
+        </Text>
+        <Card shadow="sm" radius="md" p="lg">
+          <Text c="dimmed" size="xs" fw={600} ta="center">
+            You haven't{" "}
+            <Text span inherit c="red">
+              sold
+            </Text>{" "}
+            any shares.
+          </Text>
+        </Card>
+      </>
+    );
+  }
+
   return (
     <>
       <Text fw={700} size={rem(22)}>
@@ -34,28 +55,39 @@ export function TradesHeatMap() {
             <Group key={`${s.year}`} justify="center" grow>
               <Text>{s.year}</Text>
               {s.data.map((m) => (
-                <Tooltip key={`${m.month}-${s.year}`} label={m.profit === 0 ? 'No data found' : format.toCurrency(m.profit)}>
+                <Tooltip
+                  key={`${m.month}-${s.year}`}
+                  label={
+                    m.profit === 0
+                      ? "No data found"
+                      : format.toCurrency(m.profit)
+                  }
+                >
                   <Box
                     style={{
-                      display: 'flex',
-                      alignItems: 'center',
-                      justifyContent: 'center',
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
                       height: 35,
-                      padding: '10px',
+                      padding: "10px",
                       backgroundColor:
                         m.profit === 0
-                          ? 'var(--mantine-color-gray-8)'
+                          ? "var(--mantine-color-gray-8)"
                           : m.profit > 0
-                            ? 'var(--mantine-color-teal-9)'
-                            : 'var(--mantine-color-red-9)',
-                      borderRadius: '5px',
-                      cursor: 'pointer',
-                      minWidth: '50px'
-                    }}>
+                            ? "var(--mantine-color-teal-9)"
+                            : "var(--mantine-color-red-9)",
+                      borderRadius: "5px",
+                      cursor: "pointer",
+                      minWidth: "50px",
+                    }}
+                  >
                     {m.profit === 0 ? (
                       <IconCircle size="18px" />
                     ) : (
-                      <Text fz="xs" fw={400}>{`${m.profit > 0 ? '+' : ''}${format.toHumanizedCurrency(m.profit)}`}</Text>
+                      <Text
+                        fz="xs"
+                        fw={400}
+                      >{`${m.profit > 0 ? "+" : ""}${format.toHumanizedCurrency(m.profit)}`}</Text>
                     )}
                   </Box>
                 </Tooltip>
@@ -65,7 +97,11 @@ export function TradesHeatMap() {
           <Group align="center" grow>
             <Box style={{ width: 50 }} />
             {monthShortNames.map((name) => (
-              <Text key={name} size="xs" style={{ minWidth: 50, textAlign: 'center' }}>
+              <Text
+                key={name}
+                size="xs"
+                style={{ minWidth: 50, textAlign: "center" }}
+              >
                 {name}
               </Text>
             ))}
