fa392b1dcc70120b4bbccfebe42ddb31f64ff1fb
candogusyilmaz <candogusyilmaz@gmail.com>
2025-03-26 22:52:42 +0300

    refactor: remove unused async job service and import stocks endpoint from StockController

--- src/main/java/dev/canverse/stocks/rest/stock/StockController.java
+++ src/main/java/dev/canverse/stocks/rest/stock/StockController.java
@@ -1,13 +1,11 @@
 package dev.canverse.stocks.rest.stock;
 
 import dev.canverse.stocks.domain.common.SelectItem;
-import dev.canverse.stocks.service.AsyncJobService;
 import dev.canverse.stocks.service.stock.StockService;
 import dev.canverse.stocks.service.stock.model.Stocks;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,8 +18,6 @@ import java.util.Optional;
 @RequestMapping("/api/stocks")
 public class StockController {
     private final StockService stockService;
-    private final AsyncJobService asyncJobService;
-
 
     @GetMapping
     public Stocks fetchStocks(String exchange) {
@@ -31,10 +27,5 @@ public class StockController {
     @GetMapping("/lookup")
     public List<SelectItem> fetchLookupStocks(Optional<String> exchange) {
         return stockService.fetchLookupStocks(exchange);
-    }
-
-    @PostMapping("/import")
-    public void importStocks() {
-        asyncJobService.runImportStocksJobAsync();
     }
 }
