28423b1ea96e6bb1a265d5b924c0abc132136816
candogusyilmaz <candogusyilmaz@gmail.com>
2026-09-14 16:18:03 +0100

    feat: simplify animation frame handling in RouteDrawer component

--- web/src/shared/components/route-drawer/route-drawer.tsx
+++ web/src/shared/components/route-drawer/route-drawer.tsx
@@ -16,18 +16,8 @@ export function RouteDrawer({ children, title, desktopSize, onExited }: RouteDra
   const [opened, setOpened] = useState(false);
 
   useEffect(() => {
-    let secondFrame = 0;
-
-    const firstFrame = requestAnimationFrame(() => {
-      secondFrame = requestAnimationFrame(() => {
-        setOpened(true);
-      });
-    });
-
-    return () => {
-      cancelAnimationFrame(firstFrame);
-      cancelAnimationFrame(secondFrame);
-    };
+    const frame = requestAnimationFrame(() => setOpened(true));
+    return () => cancelAnimationFrame(frame);
   }, []);
 
   const close = () => setOpened(false);
